62d20d56e1928152788e17598f53ef8dad7534df
Aboubakr Deramchi <aboubakarderamchi@gmail.com>
2022-11-15 19:04:36 +0100

    Add files via upload

--- pages/contact.js
+++ pages/contact.js
@@ -1,15 +1,13 @@
-var name =$('#name')
 
-function displayName ({
-    
 
-})
+
+
 $(document).on('click', '#submit-butt', function(){
 
  
     $("#alert-box").fadeIn()
     $(".overly").fadeIn()
-
+    displayName ()
     return false;
 })
 
@@ -17,6 +15,24 @@ $(document).on('click', '.overly, #exit-contact', function(){
 
     $("#alert-box").fadeOut()
     $(".overly").fadeOut()
-
+ 
 
 })
+
+function displayName (){
+    var contactName =$('#name').val();
+    var contactEmail =$('#email').val();
+    var html;
+    html =`
+    <div>
+        <span id="exit-contact" class="material-symbols-outlined text-blue pointer">
+        close
+        </span>
+        <div class="p10 text-blue text-center">
+        Merci ${contactName} pour votre message. <br><br><br>
+        Un message sera envoyer à votre adresse ' ${contactEmail} ' dans les prochaines 24h.
+        </div>
+    </div>
+    `
+    $("#alert-box").html(html)
+}
